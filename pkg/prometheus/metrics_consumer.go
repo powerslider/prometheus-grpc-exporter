b96ad37cc89bedb9917cc3af40fb70a06434b24f
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2021-04-08 02:25:05 +0300

    Implement metrics consumer endpoint

--- pkg/prometheus/metrics_consumer.go
+++ pkg/prometheus/metrics_consumer.go
@@ -1,21 +1,44 @@
 package prometheus
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/prometheus/prometheus/prompb"
 )
 
+var CurrentMetrics []prompb.TimeSeries
+
+type MetricsStatus struct {
+	Msg          string    `json:"message"`
+	LastModified time.Time `json:"last_modified"`
+}
+
 func ConsumeMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := json.Marshal(r.Body)
-	log.Println(string(resp))
+	var metrics []prompb.TimeSeries
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	reqBodyBytes := buf.Bytes()
+	log.Println(buf.String())
+	err := json.Unmarshal(reqBodyBytes, &CurrentMetrics)
 	if err != nil {
-		log.Fatal("error serializing metrics response: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		log.Fatal("error deserializing metrics response: ", err)
 		return
 	}
-
-	if _, err := w.Write(resp); err != nil {
+	log.Println(metrics)
+	currentStatus := MetricsStatus{Msg: "New metrics added.", LastModified: time.Now()}
+	currentStatusJson, err := json.Marshal(currentStatus)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal("error serializing metrics status: ", err)
+		return
+	}
+	if _, err := w.Write(currentStatusJson); err != nil {
+		w.WriteHeader(http.StatusCreated)
 		log.Fatal("metrics server error: ", err)
 		return
 	}
